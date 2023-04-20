2df4467cacac8891508df7350294014555ac4b16
bradfiller64 <fillerbrad64@gmail.com>
2023-04-19 17:29:35 -0700

    started app.ts setup - added cors as a dependecy

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
+import express from 'express'
+import morgan from 'morgan';
 
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const cors = require('cors');
+const corsOptions = {
+    origin: ['http://localhost:3001']
+};
+app.use(cors(corsOptions));
+
+// routes will go here
+
+app.listen(3000);
