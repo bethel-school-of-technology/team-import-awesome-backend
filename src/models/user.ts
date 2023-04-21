e4a3e56cef4d870f718bad82e0798f9cd8b6e4d8
bradfiller64 <fillerbrad64@gmail.com>
2023-04-21 14:56:57 -0700

    user model

---
+++ src/models/user.ts
@@ -0,0 +1,70 @@
+import { InferAttributes, InferCreationAttributes, Model, DataTypes, Sequelize } from "sequelize";
+
+export class User extends Model<InferAttributes<User>, InferCreationAttributes<User>>{
+    declare userId: number;
+    declare username: string;
+    declare password: string;
+    declare firstName: string;
+    declare lastName: string;
+    declare age: number;
+    declare email: string;
+    declare bio: string;
+    declare createdAt?: Date;
+    declare updatedAt?: Date;
+}
+
+export function UserFactory(sequelize: Sequelize) {
+    User.init({
+        userId: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            unique: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        age: {
+            type: DataTypes.NUMBER,
+            allowNull: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        bio: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+    }, {
+        tableName: 'users',
+        freezeTableName: true,
+        sequelize
+    });
+}
