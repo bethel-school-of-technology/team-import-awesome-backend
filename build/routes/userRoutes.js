d33ea3907b2ed78069097bef44878aa053e7decd
tmurphytown <murphytown@gmail.com>
2023-04-20 21:17:49 -0700

    Routes

---
+++ build/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { createUser, getUser, loginUser, updateUser } from '../controllers/userController';
+const router = Router();
+router.post('/', createUser);
+router.post('/login', loginUser);
+//we might have to change username to userId depeding on database schema
+router.get('/:username', getUser);
+router.put('/edit/:username', updateUser);
+export default router;
